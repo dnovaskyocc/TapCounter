52f8eb9e8627fd6f1b79b69eec8da8e268d8a0b7
dnovaskyocc <donnov48@gmail.com>
2016-09-06 21:29:52 -0700

    added javadoc comments

--- app/src/main/java/edu/orangecoastcollege/cs273/dnovasky/tapcounter/Counter.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dnovasky/tapcounter/Counter.java
@@ -2,17 +2,26 @@ package edu.orangecoastcollege.cs273.dnovasky.tapcounter;
 
 /**
  * Created by dnova_000 on 9/5/2016.
- * This class keeps track of he number of taps
+ * This class represents a counter that keeps track of the number
+ * of times the user taps the 'TAP' button
  * @author Donald Novasky
  */
 public class Counter {
 
+
     private int mTapCount;
 
+    /**
+     * This accessor returns the current tap count
+     * @return the current tap count
+     */
     public int getTapCount() {
         return mTapCount;
     }
 
+    /**
+     *  This method increments the current tap
+     */
     public void addTapCount() {
         mTapCount++;
     }
